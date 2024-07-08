350a3cb2bc5a622ce94584e3536ea1fea4aab982
Kitro7899 <g0rnostaevv@yandex.ru>
2024-07-08 23:47:58 +0300

    feat: userFilter

--- src/main/java/com/example/CodeGuardian/repository/taskRepo.java
+++ src/main/java/com/example/CodeGuardian/repository/taskRepo.java
@@ -1,9 +1,11 @@
 package com.example.CodeGuardian.repository;
 
 import com.example.CodeGuardian.Entity.task;
+import com.example.CodeGuardian.Entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 
 public interface taskRepo extends CrudRepository<task, Long> {
-
+    List<task> findByAuthor(User author);
 }
